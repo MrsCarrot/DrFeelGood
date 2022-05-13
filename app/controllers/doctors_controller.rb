380a2b8d8ec5b0b9c77715ae3e2e4ae127b5bc96
MrsCarrot <alina.zhydkova@gmail.com>
2022-05-13 15:36:59 +0300

    correction

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,49 @@
+class DoctorsController < ApplicationController
+    before_action :find_doctor, except: [:index, :new, :create]
+    
+    before_action :authenticate_user!, except: [:index, :show]
+    
+    def index
+      
+        @doctors = Doctor.all
+        
+    end
+    def new
+        @doctor = Doctor.new
+    end
+
+    def show
+        
+    end
+    
+    def create
+        #binding.pry
+        @doctor = Doctor.create!(permitted_params)
+        redirect_to doctors_path
+    end
+    def update
+        @doctor.update(permitted_params)
+        
+        if @doctor.update(permitted_params)
+          redirect_to(@doctor)
+        else
+          render "edit"
+        end
+    end
+    def destroy
+        @doctor.delete
+        redirect_to root_path
+    end
+    def edit
+        
+    end
+
+    private 
+    def find_doctor 
+        @doctor = Doctor.find_by(id: params[:id])
+    end
+
+    def permitted_params
+        params.require(:doctor).permit(:name, :description, :email, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   post '/appointment/new', to: 'appointment#create' 
 
   post '/user_session', to: 'sessions#create'
-
+  
   root 'pages#index'
   resources :users, only: [:show, :edit, :delete]
-  resources :doctors, only: [:show, :edit, :delete]
+  resources :doctors#, only: [:show, :edit, :delete]
   resources :appointments #only: [:show, :edit, :delete]
 end
